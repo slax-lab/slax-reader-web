76d639258adcc39ad2b121c2d0417b11e1edb64c
Whyjsee <1013905008@qq.com>
2025-03-17 09:11:38 +0800

    feat: rm env

--- apps/slax-reader-dweb/layer/base/nuxt.config.ts
+++ apps/slax-reader-dweb/layer/base/nuxt.config.ts
@@ -1,16 +1,8 @@
-import { getDWebConfig, getEnv } from '../../../../configs/env'
-import replace from '@rollup/plugin-replace'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 
 const currentDir = dirname(fileURLToPath(import.meta.url))
-const env = getEnv()
-const isDev = env === 'development'
-const isPreview = env === 'preview'
-console.log('Current env is:', env)
-
 console.log('path: #', join(currentDir, './assets/images'))
-const envConfig = getDWebConfig()
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
