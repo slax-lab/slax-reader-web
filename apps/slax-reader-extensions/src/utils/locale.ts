9cc2b6841464bb004046bcc124db1b158b0c8e61
Whyjsee <1013905008@qq.com>
2025-04-02 14:17:01 +0800

    feat: update locale

--- apps/slax-reader-extensions/src/utils/locale.ts
+++ apps/slax-reader-extensions/src/utils/locale.ts
@@ -1,5 +1,6 @@
-import type { GeneratedI18nStructure } from '#i18n'
+import { type GeneratedI18nStructure, i18n } from '#i18n'
 
-export const $t = (name: keyof GeneratedI18nStructure) => {
-  return i18n.t(name)
+export const $t = (name: keyof GeneratedI18nStructure, params?: string[]) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return i18n.t(name as any, params as any)
 }
