d98b95cf71650cd079f85f2c378ee84a4f5980c2
Whyjsee <1013905008@qq.com>
2025-05-06 09:55:44 +0800

    ✨ refactor selection modal to improve panel existence check and streamline menu handling

--- apps/slax-reader-extensions/src/components/Selection/selection.ts
+++ apps/slax-reader-extensions/src/components/Selection/selection.ts
@@ -85,24 +85,28 @@ export class ArticleSelection {
   private async handleMouseUp(e: MouseEvent | TouchEvent) {
     this.monitor.clearMouseListenerTry()
 
-    const selection = window.getSelection()
-    if (!selection || !selection.rangeCount) {
-      this.manager.updateCurrentMarkItemInfo(null)
-      return
-    }
-
-    const range = selection.getRangeAt(0)
-    const { list, approx } = this.manager.getElementInfo(range)
-
-    if (!list || list.length === 0 || !approx) {
-      this.manager.updateCurrentMarkItemInfo(null)
-      return
-    }
-
-    const source = this.getMarkPathItems(list)
-    if (!source) return
-
     setTimeout(() => {
+      if (SelectionModal.isPanelExist(this.config.containerDom!)) {
+        return
+      }
+
+      const selection = window.getSelection()
+      if (!selection || !selection.rangeCount) {
+        this.manager.updateCurrentMarkItemInfo(null)
+        return
+      }
+
+      const range = selection.getRangeAt(0)
+      const { list, approx } = this.manager.getElementInfo(range)
+
+      if (!list || list.length === 0 || !approx) {
+        this.manager.updateCurrentMarkItemInfo(null)
+        return
+      }
+
+      const source = this.getMarkPathItems(list)
+      if (!source) return
+
       const markInfoItem = this.manager.getMarkItemInfos().find(infoItem => this.manager.checkMarkSourceIsSame(infoItem.source, source))
       if (markInfoItem) {
         this.manager.updateCurrentMarkItemInfo(markInfoItem)
