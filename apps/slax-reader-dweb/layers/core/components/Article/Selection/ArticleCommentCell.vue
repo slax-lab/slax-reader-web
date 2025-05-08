04a461bc618b99ff6f82f5e455552a95f60c0f9b
Whyjsee <1013905008@qq.com>
2025-05-08 13:58:00 +0800

    ✨ enhance comment functionality with dark mode support and add selection menus

--- apps/slax-reader-dweb/layers/core/components/Article/Selection/ArticleCommentCell.vue
+++ apps/slax-reader-dweb/layers/core/components/Article/Selection/ArticleCommentCell.vue
@@ -15,8 +15,15 @@
         <span class="date">{{ showCreateTime(comment) }}</span>
         <div class="operates" v-if="comment.markId !== 0 && !comment.loading">
           <template v-if="!comment.operateLoading">
-            <button class="reply-btn group-hover/comment:!opacity-100" @click="replyComment(comment)"></button>
-            <button class="delete-btn group-hover/comment:!opacity-100" v-if="canDeleteComment(comment)" @click="commentDeleteClick(comment)"></button>
+            <button
+              class="bg-[url('@images/tiny-comment-icon.png')] dark:bg-[url('@images/tiny-comment-icon-dark.png')] group-hover/comment:!opacity-100"
+              @click="replyComment(comment)"
+            ></button>
+            <button
+              class="bg-[url('@images/tiny-delete-red-outline-icon.png')] group-hover/comment:!opacity-100"
+              v-if="canDeleteComment(comment)"
+              @click="commentDeleteClick(comment)"
+            ></button>
           </template>
           <div class="i-svg-spinners:180-ring-with-bg ml-10px text-16px text-#999" v-else-if="comment.operateLoading"></div>
         </div>
@@ -39,9 +46,12 @@
             <span class="date">{{ showCreateTime(comment) }}</span>
             <div class="operates" v-if="childComment.markId !== 0 && !childComment.loading">
               <template v-if="!childComment.operateLoading">
-                <button class="reply-btn group-hover/child:!opacity-100" @click="replyComment(childComment)"></button>
                 <button
-                  class="delete-btn group-hover/child:!opacity-100"
+                  class="bg-[url('@images/tiny-comment-icon.png')] dark:bg-[url('@images/tiny-comment-icon-dark.png')] group-hover/child:!opacity-100"
+                  @click="replyComment(childComment)"
+                ></button>
+                <button
+                  class="bg-[url('@images/tiny-delete-red-outline-icon.png')] group-hover/child:!opacity-100"
                   v-if="!childComment.isDeleted && canDeleteComment(childComment)"
                   @click="commentDeleteClick(childComment)"
                 ></button>
@@ -141,7 +151,8 @@ const postComment = (comment: MarkCommentInfo, replyComment: string) => {
 
 <style lang="scss" scoped>
 .article-comment-cell {
-  --style: 'px-16px pt-16px pb-20px rounded-8px bg-#fff not-first:mt-8px';
+  --style: 'px-16px pt-16px pb-20px rounded-8px not-first:mt-8px';
+  --style: 'bg-#fff dark:bg-#333333FF';
 
   .comment-header {
     --style: flex justify-between items-center;
@@ -153,7 +164,8 @@ const postComment = (comment: MarkCommentInfo, replyComment: string) => {
       }
 
       span {
-        --style: ml-8px text-(13px #999) line-height-18px;
+        --style: ml-8px text-(13px) line-height-18px;
+        --style: 'text-#999 dark:text-#ffffff66';
       }
     }
 
@@ -167,28 +179,22 @@ const postComment = (comment: MarkCommentInfo, replyComment: string) => {
   }
 
   .comment-content {
-    --style: text-(16px #333) line-height-24px whitespace-pre-line;
+    --style: text-(16px) line-height-24px whitespace-pre-line;
+    --style: 'text-#333 dark:text-#ffffffcc';
   }
 
   .comment-footer {
     --style: mt-4px flex items-center justify-between;
 
     .date {
-      --style: text-(13px #999) line-height-18px select-none;
+      --style: text-(13px) line-height-18px select-none;
+      --style: 'text-#999 dark:text-#999999ff';
     }
 
     .operates {
       --style: flex-center;
       button {
         --style: 'w-16px h-16px opacity-0 bg-contain not-first:(ml-10px) hover:(scale-105) active:(scale-110) transition-all duration-250';
-      }
-
-      .reply-btn {
-        background-image: url('@images/tiny-comment-icon.png');
-      }
-
-      .delete-btn {
-        background-image: url('@images/tiny-delete-red-outline-icon.png');
       }
     }
   }
@@ -202,20 +208,19 @@ const postComment = (comment: MarkCommentInfo, replyComment: string) => {
       .child-comment-content {
         --style: text-(14px) line-height-22px whitespace-pre-line;
 
-        .child-username {
-          --style: text-#999 select-none;
+        .child-username,
+        .parent-username {
+          --style: select-none;
+          --style: 'text-#999 dark:text-#FFFFFF66';
         }
 
         .reply-text {
-          --style: text-#333 select-none;
-        }
-
-        .parent-username {
-          --style: text-#999 select-none;
+          --style: select-none;
+          --style: 'text-#333 dark:text-#FFFFFFCC';
         }
 
         .comment-content-text {
-          --style: text-#333;
+          --style: 'text-#333 dark:text-#FFFFFFCC';
         }
       }
     }
--- apps/slax-reader-dweb/layers/core/components/Article/Selection/ArticleSelectionMenus.ce.vue
+++ apps/slax-reader-dweb/layers/core/components/Article/Selection/ArticleSelectionMenus.ce.vue
@@ -1,10 +1,12 @@
 <template>
   <Transition name="opacity" @after-leave="onAfterLeave">
-    <div class="article-selection-menus" v-show="appear" v-on-click-outside="handleClickOutside">
-      <button class="menu" v-for="menu in menus" :key="menu.id" @click="e => handleClick(menu.id, e)">
-        <img :src="menu.icon" />
-        <span>{{ menu.name }}</span>
-      </button>
+    <div :class="{ dark: dark }" v-show="appear">
+      <div class="article-selection-menus" v-on-click-outside="handleClickOutside">
+        <button class="menu" v-for="menu in menus" :key="menu.id" @click="e => handleClick(menu.id, e)">
+          <img :src="menu.icon" />
+          <span>{{ menu.name }}</span>
+        </button>
+      </div>
     </div>
   </Transition>
 </template>
@@ -21,7 +23,16 @@ interface MenuItem {
   icon: string
 }
 
-const props = defineProps(['allowAction'])
+const props = defineProps({
+  allowAction: {
+    type: Boolean,
+    required: false
+  },
+  dark: {
+    type: Boolean,
+    required: false
+  }
+})
 const emits = defineEmits(['action', 'dismiss', 'noAction'])
 const menus = ref<MenuItem[]>([])
 const appear = ref(false)
@@ -35,7 +46,7 @@ const updateMenus = () => {
     {
       id: MenuType.Copy,
       name: t('common.operate.copy'),
-      icon: new URL('@images/menu-copy-icon.png', import.meta.url).href
+      icon: !props.dark ? new URL('@images/menu-copy-icon.png', import.meta.url).href : new URL('@images/menu-copy-icon-dark.png', import.meta.url).href
     }
   ]
   if (props.allowAction) {
@@ -44,12 +55,12 @@ const updateMenus = () => {
         {
           id: MenuType.Stroke,
           name: t('common.operate.line'),
-          icon: new URL('@images/menu-stroke-icon.png', import.meta.url).href
+          icon: !props.dark ? new URL('@images/menu-stroke-icon.png', import.meta.url).href : new URL('@images/menu-stroke-icon-dark.png', import.meta.url).href
         },
         {
           id: MenuType.Comment,
           name: t('common.operate.comment'),
-          icon: new URL('@images/menu-comment-icon.png', import.meta.url).href
+          icon: !props.dark ? new URL('@images/menu-comment-icon.png', import.meta.url).href : new URL('@images/menu-comment-icon-dark.png', import.meta.url).href
         }
       ]
     )
@@ -58,7 +69,7 @@ const updateMenus = () => {
   menus.value.push({
     id: MenuType.Chatbot,
     name: t('common.operate.chatbot'),
-    icon: new URL('@images/menu-chatbot-icon.png', import.meta.url).href
+    icon: !props.dark ? new URL('@images/menu-chatbot-icon.png', import.meta.url).href : new URL('@images/menu-chatbot-icon-dark.png', import.meta.url).href
   })
 }
 
@@ -97,18 +108,26 @@ onUnmounted(() => {})
 </script>
 
 <style lang="scss" scoped>
+@use '#layers/core/styles/global.scss' as *;
+
 .article-selection-menus {
-  --style: p-4px bg-#fff shadow-[0px_20px_40px_0px_#00000029] rounded-8px border-(1px solid #a8b1cd33) inline-flex items-center;
+  --style: p-4px shadow-[0px_20px_40px_0px_#00000029] rounded-8px border-(1px solid #a8b1cd33) inline-flex items-center;
+  --style: ' bg-#fff dark:bg-#333333FF';
 
   .menu {
     --style: 'px-12px py-8px rounded-6px cursor-pointer flex items-center hover:(bg-#f5f5f3) active:(scale-105) transition-all duration-250';
+
+    &:hover {
+      --style: 'bg-#f5f5f3 dark:bg-#262626';
+    }
 
     img {
       --style: w-24px h-24px object-fit;
     }
 
     span {
-      --style: ml-2px text-(13px #999) line-height-18px shrink-0;
+      --style: ml-2px text-(13px) line-height-18px shrink-0;
+      --style: 'text-#999 dark:text-#ffffff66';
     }
   }
 }
