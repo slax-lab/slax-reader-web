0bfff9ace1cda43c0ede04b5083ba67cc172c4f7
yaojunchang <1013905008@qq.com>
2026-01-07 10:10:52 +0800

    🎨 Refactor code structure for improved readability and maintainability (#156)
    * Refactor code structure for improved readability and maintainability
    
    * ✨ update draggable logic to call updatePageWidthStyle for improved layout handling
    
    * ✨ bump version to 1.10.4 in package.json and wxt.config.ts
    
    * ✨ add cookie handling for original domain in auth flow

--- apps/slax-reader-dweb/layers/core/composables/useAuth.ts
+++ apps/slax-reader-dweb/layers/core/composables/useAuth.ts
@@ -39,6 +39,10 @@ const useAuth = {
 
     const $config = useNuxtApp().$config.public
     set($config.COOKIE_TOKEN_NAME, resp.token, { path: '/', expires: new Date(Date.now() + 60 * 24 * 60 * 60 * 1000), domain: `${$config.COOKIE_DOMAIN}` })
+    try {
+      checkAnd1SetOriginalCookies(resp.token)
+    } catch (e) {}
+
     return resp.token
   },
   async clearAuth(): Promise<void> {
@@ -52,6 +56,18 @@ const useAuth = {
       checkAndRemoveOriginalCookies()
     } catch (e) {}
   }
+}
+
+const checkAnd1SetOriginalCookies = (token: string) => {
+  const $config = useNuxtApp().$config.public
+  const splits = $config.COOKIE_DOMAIN.split('.').filter(token => !!token)
+  if (splits.length <= 2) {
+    return
+  }
+
+  splits[0] = ''
+  const domain = splits.slice(1).join('.')
+  set($config.COOKIE_TOKEN_NAME, token, { path: '/', expires: new Date(Date.now() + 60 * 24 * 60 * 60 * 1000), domain: `${domain}` })
 }
 
 const checkAndRemoveOriginalCookies = () => {
