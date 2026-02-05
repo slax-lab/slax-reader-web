411bed257e85669d2afbcf7eeb7549f6870e3646
YaoJunchang <yaojunchang@zsxq.com>
2026-02-05 11:07:53 +0800

    🎨 refactor app.vue and enhance SEO in [id].vue with schema.org integration

--- apps/slax-reader-dweb/layers/core/app.vue
+++ apps/slax-reader-dweb/layers/core/app.vue
@@ -4,22 +4,6 @@
   </NuxtLayout>
 </template>
 
-<script setup lang="ts">
-useSchemaOrg([
-  defineOrganization({
-    name: 'Slax Reader',
-    url: 'https://r.slax.com/en',
-    logo: 'https://r.slax.com/logo.png',
-    sameAs: ['https://x.com/SlaxReader', 'https://t.me/slax_app', 'https://github.com/slax-lab/slax-reader'],
-    contactPoint: [
-      {
-        '@type': 'ContactPoint',
-        email: 'support@slax.com',
-        contactType: 'customer support'
-      }
-    ]
-  })
-])
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss" scoped></style>
