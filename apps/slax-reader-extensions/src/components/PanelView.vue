239ce1a21df143f5e92eb86b077483d23d2361e6
YaoJunchang <yaojunchang@zsxq.com>
2026-08-07 10:42:42 +0800

    💄 Resize extension panel toggle icon

--- apps/slax-reader-extensions/src/components/PanelView.vue
+++ apps/slax-reader-extensions/src/components/PanelView.vue
@@ -398,7 +398,7 @@ const closePanel = () => {
             }
 
             button {
-              --style: ml-4px w-18px h-16px bg-center;
+              --style: ml-6px w-14px h-12px bg-center;
               --style: 'bg-[length:18px_16px] hover:(scale-103) active:(scale-105) transition-all duration-250';
 
               background-image: url('@/assets/tiny-toggle-disabled-icon.png');
